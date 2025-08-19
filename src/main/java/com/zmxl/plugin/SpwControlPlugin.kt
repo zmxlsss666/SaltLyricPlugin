85a77ea7c38130ffb7046f68ef5ea7467efe35da
zmxlsss666 <154047141+zmxlsss666@users.noreply.github.com>
2025-08-19 15:58:05 +0800

    Update SpwControlPlugin.kt

--- src/main/java/com/zmxl/plugin/SpwControlPlugin.kt
+++ src/main/java/com/zmxl/plugin/SpwControlPlugin.kt
@@ -3,28 +3,12 @@ package com.zmxl.plugin
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
 import com.zmxl.plugin.server.HttpServer
-import com.zmxl.plugin.control.SmtcController  
+import com.zmxl.plugin.control.SmtcController
+import com.zmxl.plugin.lyrics.DesktopLyrics
 
 class SpwControlPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
     private lateinit var httpServer: HttpServer
-
-    override fun start() {
-        super.start()
-        httpServer = HttpServer(35373)
-        httpServer.start()
-        SmtcController.init()  
-        println("SPW Control Plugin started with HTTP server on port 35373")
-    }
-
-    override fun stop() {
-        super.stop()
-        httpServer.stop()
-        SmtcController.shutdown()  
-        println("SPW Control Plugin stopped")
-    }
-class SpwControlPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
-    private lateinit var httpServer: HttpServer
-    private lateinit var lyricsApp: Thread
+    private var lyricsApp: DesktopLyrics? = null
 
     override fun start() {
         super.start()
@@ -33,9 +17,8 @@ class SpwControlPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
         SmtcController.init()
         
         // 启动桌面歌词应用
-        lyricsApp = thread {
-            DesktopLyrics.main(arrayOf())
-        }
+        lyricsApp = DesktopLyrics
+        lyricsApp?.start()
 
         println("SPW Control Plugin started with HTTP server on port 35373")
     }
@@ -46,10 +29,9 @@ class SpwControlPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
         SmtcController.shutdown()
         
         // 停止歌词应用
-        lyricsApp.interrupt()
+        lyricsApp?.stop()
+        lyricsApp = null
 
         println("SPW Control Plugin stopped")
     }
 }
-}
-
