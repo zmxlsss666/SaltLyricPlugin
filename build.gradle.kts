df54c9f6353f5769553879ffad4b5adeff109986
zmxlsss666 <154047141+zmxlsss666@users.noreply.github.com>
2025-08-24 14:06:02 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,24 +13,34 @@ kotlin {
     jvmToolchain(21)
 }
 
-
-
 dependencies {
     // SPW API
     compileOnly("com.github.Moriafly:spw-workshop-api:0.1.0-dev08")
     kapt("com.github.Moriafly:spw-workshop-api:0.1.0-dev08")
-
+    
     // Jetty HTTP服务器
     implementation("org.eclipse.jetty:jetty-server:11.0.15")
     implementation("org.eclipse.jetty:jetty-servlet:11.0.15")
+    
+    // 最新JNA坐标（已迁移到net.java.dev.jna）
     implementation("net.java.dev.jna:jna:5.10.0")
     implementation("net.java.dev.jna:jna-platform:5.10.0")
     implementation("org.json:json:20210307")
     // Kotlin标准库
     implementation(kotlin("stdlib-jdk8"))
-    implementation("net.jthink:jaudiotagger:3.0.1")
+    
     // JSON序列化
     implementation("com.google.code.gson:gson:2.10.1")
+    
+    // 音频文件元数据解析
+    implementation("org.apache.tika:tika-core:2.9.1")
+    implementation("com.drewnoakes:metadata-extractor:2.18.0")
+    
+    // FLAC文件解析
+    implementation("com.googlecode.soundlibs:flac:1.5.2")
+    
+    // MP3文件解析
+    implementation("com.googlecode.soundlibs:mp3spi:1.9.5.4")
 }
 val pluginClass = "com.zmxl.plugin.SpwControlPlugin"
 val pluginId = "zmxl-spw-control"
@@ -57,14 +67,3 @@ tasks.register<Jar>("plugin") {
     }
     archiveExtension.set("zip")
 }
-
-
-
-
-
-
-
-
-
-
-
